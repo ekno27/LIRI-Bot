47eeb3848081b4a9ecb13789b7323c3f68c4e1c4
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-09 21:54:12 -0700

    added my-tweets functionality

--- liri.js
+++ liri.js
@@ -15,8 +15,33 @@ var argv = process.argv;
 //obtains argument that will determine what liri will do
 var argument = argv[2];
 console.log(argument);
-//my-tweets: This will show your last 20 tweets and when they were created 
-// at in your terminal/bash window.
+
+/**
+ * Commands to carry out:
+ */
+
+//my-tweets: This will show your last 20 tweets and when they were created at in your terminal/bash window.
+if(argument ==="my-tweets"){
+    client.get("statuses/user_timeline", function(error,tweets,response){
+        if(error) throw error;
+        console.log(tweets[1].text);
+        //if there's less than 20 tweets, show 'em all, else show only 20
+        if(tweets.length<=20){
+            for(var i = 0; i<tweets.length;i++){
+                console.log("Tweet: "+  tweets[i].text);
+                console.log("Creation date: "+ tweets[i].created_at+ "\n");
+            }
+
+        }
+        else{
+            for(var i = 0;i<20;i++){
+                console.log("Tweet: "+  tweets[i].text);
+                console.log("Creation date: "+ tweets[i].created_at+ "\n");
+            }
+        }
+    })
+    
+}
 
 
 
