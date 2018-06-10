2223c4ccd7c0d248fa1e6940abd7dda6aeafbf05
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-09 22:48:58 -0700

    getting a layout of the returned object

--- liri.js
+++ liri.js
@@ -15,8 +15,33 @@ var argv = process.argv;
 //obtains argument that will determine what liri will do
 var argument = argv[2];
 console.log(argument);
+
+//obtaining the query 
+var queryPrompt ="";
+for (var i = 3;i< argv.length;i++){
+    queryPrompt += argv[i]+ " "; 
+}
+queryPrompt = queryPrompt.substring(0,queryPrompt.length-1);
+console.log(queryPrompt);
 //my-tweets: This will show your last 20 tweets and when they were created 
 // at in your terminal/bash window.
 
-
+//2. node liri.js spotify-this-song '<song name here>'
+// This will show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+if (argument = "spotify-this-song"){
+    spotify
+    .search({ type: 'track', query: queryPrompt })
+    .then(function(response) {
+      console.log(response.tracks.items[1].artists);
+      var artists="";
+      
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
 
